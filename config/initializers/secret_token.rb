f9c51f63b1163b9bd568b858bfe9b631ab2c0373
Doug Stewart <doug.stewart@ama.ab.ca>
2010-12-22 20:34:53 -0700

    Wednesday

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Istodaytuesday::Application.config.secret_token = '072559d9a85585d0fc3df3c4fd0f8fb01bc6fbc3da9d70e9e0e15dda49687c76f56a0c69c3dcef89fdea18eef245a7b63bf25274cbf44979c9e2d2bb7ec3c263'
+Istodaywednesday::Application.config.secret_token = '072559d9a85585d0fc3df3c4fd0f8fb01bc6fbc3da9d70e9e0e15dda49687c76f56a0c69c3dcef89fdea18eef245a7b63bf25274cbf44979c9e2d2bb7ec3c263'
