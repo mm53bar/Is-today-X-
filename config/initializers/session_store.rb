ea131dbb7de616777940580bd226f9e972532c9e
Doug Stewart <doug.stewart@ama.ab.ca>
2010-12-22 20:50:29 -0700

    Monday

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Istodaysunday::Application.config.session_store :cookie_store, :key => '_istodaysunday_session'
+Istodaymonday::Application.config.session_store :cookie_store, :key => '_istodaymonday_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Istodaysunday::Application.config.session_store :active_record_store
+# Istodaymonday::Application.config.session_store :active_record_store
