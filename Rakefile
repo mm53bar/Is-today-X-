89a6951c9927d69d7d1109ec4a524d14867a3551
Doug Stewart <doug.stewart@ama.ab.ca>
2010-12-30 07:20:16 -0700

    Friday update

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 require File.expand_path('../config/application', __FILE__)
 require 'rake'
 
-Istodaythursday::Application.load_tasks
+Istodayfriday::Application.load_tasks
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Istodaythursday::Application.configure do
+Istodayfriday::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
