abc0553b48bb405dcf7100fe3100b4fcb0b9f171
Doug Stewart <doug.stewart@ama.ab.ca>
2011-04-22 22:57:28 -0600

    Update for Thursday

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
   
   def index
-    @day = "Wednesday"
+    @day = "Thursday"
     @slug = "Is today " + @day + "?"
     
     respond_to do |format|
